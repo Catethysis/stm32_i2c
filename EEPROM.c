274fd670e552ebc80c74cbf48426f3251e763ec7
Catethysis <Catethysis@gmail.com>
2014-07-03 15:36:22 +0400

    Исправлен адрес записи в примере

--- EEPROM.c
+++ EEPROM.c
@@ -13,7 +13,7 @@ void main()
 	I2C_burst_write(I2C1, 0xA0, 0x11, 5, burst_data_write);
 		delay();
 	
-	single_data_read = I2C_single_read(I2C1, 0xA0, 0x020);
+	single_data_read = I2C_single_read(I2C1, 0xA0, 0x20);
 		delay();
 	I2C_burst_read(I2C1, 0xA0, 0x11, 5, burst_data_read);
 	
